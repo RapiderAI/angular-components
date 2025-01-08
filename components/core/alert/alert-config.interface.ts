ee93e5ed4253be7370736587ae8fad852424ba89
canmustu <mustuserefcan@gmail.com>
2025-01-08 22:48:21 +0300

    fix(alert): import fix

--- components/core/alert/alert-config.interface.ts
+++ components/core/alert/alert-config.interface.ts
@@ -1,7 +1,6 @@
 import { ButtonComponentConfig } from '@rapider/angular-components/core/button';
 import { HeadingComponentConfig } from '@rapider/angular-components/core/heading';
 import { TextComponentConfig } from '@rapider/angular-components/core/text';
-import { AlertActionConfigPlacement, AlertType } from '@rapider/angular-components/core/alert';
 import {
   BorderConfig,
   BoxShadowConfig,
@@ -9,6 +8,8 @@ import {
   SizeConfig,
   SpacingConfig,
 } from '@rapider/angular-components/core/style';
+import { AlertActionConfigPlacement } from './alert-action-config-placement.enum';
+import { AlertType } from './alert-type.enum';
 
 
 export interface AlertConfig {
