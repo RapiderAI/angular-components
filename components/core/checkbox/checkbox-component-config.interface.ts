87cb22d3a5b257db472f8bebefde489af6a4e5b2
Alice Büşra Alçınar アリス <busralcinar@gmail.com>
2025-01-21 14:31:26 +0300

    migration/checkbox-component

---
+++ components/core/checkbox/checkbox-component-config.interface.ts
@@ -0,0 +1,20 @@
+import { BorderConfig, BoxShadowConfig, SizeConfig, SpacingConfig } from '@rapider/angular-components/core/style';
+import { IconComponentConfig } from'@rapider/angular-components/icon';
+import { CheckboxType } from './checkbox-type.enum';
+import { TextComponentConfig } from '@rapider/angular-components/text';
+
+export interface CheckboxComponentConfig {
+  text: TextComponentConfig;
+  icon: IconComponentConfig;
+  disabled?: boolean;
+  cssStyle: { [key: string]: any };
+  cssClass: string;
+  checkboxType: CheckboxType;
+  tooltip: string;
+  additionalIcon: IconComponentConfig;
+  borderSettings: BorderConfig;
+  customSizeSettings: SizeConfig;
+  shadowSettings: BoxShadowConfig;
+  paddingSettings: SpacingConfig;
+  marginSettings: SpacingConfig;
+}
--- runner/src/app/pages/page1.component.ts
+++ runner/src/app/pages/page1.component.ts
@@ -20,6 +20,7 @@ import { FormsModule } from '@angular/forms';
 import { RappiderSelectModule } from '@rapider/angular-components/select/select.module';
 import { RappiderSpinComponent } from '@rapider/angular-components/spin';
 import { SelectComponentConfig } from '@rapider/angular-components/select';
+import { RappiderCheckboxComponent } from '@rapider/angular-components/checkbox/checkbox.component';
 
 @Component({
   standalone: true,
@@ -41,6 +42,7 @@ import { SelectComponentConfig } from '@rapider/angular-components/select';
     RappiderRateComponent,
     RappiderSelectModule,
     RappiderSpinComponent,
+    RappiderCheckboxComponent
     // RappiderBadgeComponent,
   ],
   selector: 'app-page',
@@ -68,6 +70,7 @@ import { SelectComponentConfig } from '@rapider/angular-components/select';
       <rappider-rate [(ngModel)]="rate" (valueChange)="onChange($event)"></rappider-rate>
       <rappider-spin [spinning]="true">helo</rappider-spin>
       <rappider-select [options]="select.options" [ngModel]="'turkish-airlines'" optionMode="options"></rappider-select>
+      <rappider-checkbox [text]="text"></rappider-checkbox>
     -->
 
   </div>
