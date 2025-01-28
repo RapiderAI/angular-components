81d8392775475d40ef21e6e06e7e3f7fc260b78d
Caner Eğin <cnrgn96@gmail.com>
2025-01-28 18:22:59 +0300

    input label

--- runner/src/app/pages/page1.component.ts
+++ runner/src/app/pages/page1.component.ts
@@ -29,6 +29,7 @@ import { RappiderCheckboxListComponent } from '@rapider/angular-components/check
 import { RappiderTextboxComponent } from '@rapider/angular-components/textbox';
 import { RappiderPaginationComponent } from '@rapider/angular-components/pagination';
 import { RappiderBlockquoteComponent } from '@rapider/angular-components/blockquote';
+import { RappiderInputLabelComponent } from '@rapider/angular-components/input-label/input-label.component';
 
 @Component({
   standalone: true,
@@ -55,7 +56,8 @@ import { RappiderBlockquoteComponent } from '@rapider/angular-components/blockqu
     RappiderPaginationComponent,
     RappiderBlockquoteComponent,
     // RappiderBadgeComponent,
-    RappiderProgressComponent
+    RappiderProgressComponent,
+    RappiderInputLabelComponent
   ],
   selector: 'app-page',
   template: `
@@ -86,12 +88,12 @@ import { RappiderBlockquoteComponent } from '@rapider/angular-components/blockqu
       <rappider-progress [percent]="90" [showInfo]="true" [status]="'normal'" [type]="'dashboard'" [successPercent]="50" [width]="150" [strokeWidth]="10" [isSuccessPercentVisible]="true" [paddingSettings]="paddingSettings" [marginSettings]="marginSettings"></rappider-progress>
       <rappider-checkbox-list [options]="options"></rappider-checkbox-list>
       <rappider-blockquote [quote]="blockquote.quote" [footer]="blockquote.footer"></rappider-blockquote>
+  <rappider-input-label [title]="'i am input label'" [description]="'i am description'" [icon]="{name: 'fa-solid fa-user-group',type: 'FONT_AWESOME'}"></rappider-input-label>
     -->
 
   </div>
 
   <div style="border: 1px solid black; padding: 30px; margin: 5px;">
-    
   </div>
   `,
 })
