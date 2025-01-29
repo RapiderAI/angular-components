15fb3823a7a4f4139401901f95c6cd4cbbb10e4b
MustafaDemirhan <mustafaa.demirhann.0@gmail.com>
2025-01-29 11:25:54 +0300

    migration/json-input-component

---
+++ components/json-input/json-input.component.ts
@@ -0,0 +1,84 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output, forwardRef } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { RappiderButtonModule } from '../button/button.module';
+
+@Component({
+  selector: 'rappider-json-input',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RappiderButtonModule,
+  ],
+  templateUrl: './json-input.component.html',
+  styleUrls: ['./json-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RappiderJsonInputComponent),
+      multi: true
+    }
+  ]
+})
+export class RappiderJsonInputComponent implements ControlValueAccessor {
+
+  @Input() editorOptions: any = {
+    theme: 'vs-default',
+    language: 'json',
+    minimap: { autohide: true, enabled: false },
+    lineNumbers: { renderType: 0 }
+  };
+
+  @Output() blur = new EventEmitter<any>();
+
+  _value: any;
+  localeValue: string;
+
+  get value() {
+    return this._value;
+  }
+
+  set value(value: any) {
+    this._value = value;
+    this.localeValue = JSON.stringify(this.value);
+    this.onChange(value);
+    this.onTouched();
+    this.blur.emit(value);
+  }
+
+  onChange: any = () => { };
+  onTouched: any = () => { };
+
+  writeValue(value: Record<string, any>): void {
+    if (value) {
+      this._value = value;
+      this.localeValue = JSON.stringify(this.value);
+    } else {
+      this._value = null;
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  onCodemirrorValueChange(value) {
+    this.localeValue = value;
+  }
+
+
+  onCodemirrorSave() {
+    try {
+      this.value = JSON.parse(this.localeValue);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+}
