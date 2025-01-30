91e44cd276c7e25339aa62bf3c2c8bf3c34f226d
canmustu <mustuserefcan@gmail.com>
2025-01-30 21:24:47 +0300

    feat(icon-and-menu-list): component has been added

---
+++ components/icon-and-menu-list/icon-and-menu-list.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { RappiderIconAndMenuListComponent } from './icon-and-menu-list.component';
+
+@NgModule({
+  imports: [RappiderIconAndMenuListComponent],
+  exports: [RappiderIconAndMenuListComponent]
+})
+export class RappiderIconAndMenuListModule { }
---
+++ components/icon-and-menu-list/utils/index.ts
@@ -0,0 +1,2 @@
+export * from './icon-and-menu-list-item-component-config.interface';
+export * from './icon-and-menu-list-component-config.interface';
