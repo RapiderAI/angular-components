5aef7a0c4001cbf0dc532d7b09dba7614da34237
canmustu <mustuserefcan@gmail.com>
2025-01-31 22:55:23 +0300

    feat(cart-item): component has been added

---
+++ components/core/cart-item/cart-item-additional-content.interface.ts
@@ -0,0 +1,7 @@
+import { ButtonComponentConfig } from '@rapider/angular-components/button';
+import { IconBlockComponentConfig } from '@rapider/angular-components/icon-block';
+
+export interface CartItemAdditionalContent {
+  iconBlock?: IconBlockComponentConfig;
+  button?: ButtonComponentConfig;
+}
