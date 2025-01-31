298bca39723ccf5639eb4e26a6fa47db2bb654c1
canmustu <mustuserefcan@gmail.com>
2025-01-31 21:04:29 +0300

    feat(menu): component has been added

---
+++ components/menu/utils/menu-component-config.interface.ts
@@ -0,0 +1,8 @@
+import { MenuConfig, MenuPlacement } from '@rapider/angular-components/core/menu';
+
+export interface MenuComponentConfig {
+  config: MenuConfig;
+  isCollapsed?: boolean;
+  paddingValue?: number;
+  menuPlacement?: MenuPlacement;
+}
