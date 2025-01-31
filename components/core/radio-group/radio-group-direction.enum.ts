6674b0a28c50615541ffb857d4fb741502c0ec7e
Alice Büşra Alçınar アリス <busralcinar@gmail.com>
2025-01-31 14:55:50 +0300

    migration/radio-group

---
+++ components/core/radio-group/radio-group-direction.enum.ts
@@ -0,0 +1,15 @@
+export enum RadioGroupDirection {
+  Horizantal = 'horizantal',
+  Vertical = 'vertical'
+}
+
+export const radioGroupDirectionOptions = [
+  {
+    key: 'Horizantal',
+    value: 'horizontal'
+  },
+  {
+    key: 'Vertical',
+    value: 'vertical'
+  }
+];
---
+++ components/core/radio-group/radio-group-options.interface.ts
@@ -0,0 +1,12 @@
+import { IconComponentConfig } from "@rapider/angular-components/icon";
+
+export interface RadioGroupOptions {
+  value: any;
+  label?: string;
+  key?: string;
+  labelSize?: string;
+  icon?: IconComponentConfig;
+  iconSize?: string;
+  tooltip?: string;
+  customHTML?: string;
+}
