7f43a36fdbd8a6fb6edd7b4fb755161af63a134c
MustafaDemirhan <mustafaa.demirhann.0@gmail.com>
2025-01-31 15:03:43 +0300

    delete modal

--- components/modal/modal.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RappiderModalComponent } from './modal.component';
-
-@NgModule({
-  imports: [RappiderModalComponent],
-  exports: [RappiderModalComponent]
-})
-export class RappiderModalModule { }
