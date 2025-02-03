cd51b6da53b56bef35b5a9232d520d33f2794ba1
MustafaDemirhan <mustafaa.demirhann.0@gmail.com>
2025-02-03 15:25:30 +0300

    fix

--- runner/src/app/pages/page1.component.ts
+++ runner/src/app/pages/page1.component.ts
@@ -2,10 +2,6 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { RappiderDrawerComponent } from '@rapider/angular-components/drawer';
-import { RappiderAddressFormComponent } from '@rapider/angular-components/address-form';
-import { AddressFormData } from '@rapider/angular-components/core/address-form';
-import { ButtonType } from '@rapider/angular-components/core/button';
-import { ButtonComponentConfig } from '@rapider/angular-components/button';
 
 @Component({
   standalone: true,
@@ -13,7 +9,6 @@ import { ButtonComponentConfig } from '@rapider/angular-components/button';
     FormsModule,
     RouterModule,
     RappiderDrawerComponent,
-    RappiderAddressFormComponent,
   ],
   selector: 'app-page',
   template: `
@@ -23,28 +18,12 @@ import { ButtonComponentConfig } from '@rapider/angular-components/button';
 
   <div style="border: 1px solid black; padding: 30px; margin: 5px;">
 
-    <rappider-address-form [data]="addressFormData" [submitButton]="submitButton"></rappider-address-form>
-
 
   </div>
   `,
 })
 
 export class Page1Component {
-
-  addressFormData: AddressFormData = {
-
-    street: '123 Main St',
-    city: 'Anytown',
-    state: 'CA',
-    zip: '12345'
-
-  };
-
-  submitButton: ButtonComponentConfig = {
-    text: 'Submit',
-    type: ButtonType.Default,
-  };
 
   menu = {
     mode: 'horizontal',
