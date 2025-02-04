d1a893eb7d6f6b915678d407b86bea3145c522eb
MustafaDemirhan <mustafaa.demirhann.0@gmail.com>
2025-02-04 14:46:00 +0300

    update DynamicDataForSelectBox

--- components/core/edit-form/dynamic-data-for-selectbox.interface.ts
+++ components/core/edit-form/dynamic-data-for-selectbox.interface.ts
@@ -1,5 +1,5 @@
-import { SelectableOption } from '../form-utils';
-import { GrouppedOption } from '../select';
+import { SelectableOption } from '@rapider/angular-components/core/form-utils';
+import { GrouppedOption } from '@rapider/angular-components/core/select';
 
 export interface DynamicDataForSelectBox {
   fieldName: string;
