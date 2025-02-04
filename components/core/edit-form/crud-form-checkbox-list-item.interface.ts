24237945590abaacecf51bde1729d93620a86f02
MustafaDemirhan <mustafaa.demirhann.0@gmail.com>
2025-02-04 14:05:51 +0300

    update CrudFormCheckboxListItem

--- components/core/edit-form/crud-form-checkbox-list-item.interface.ts
+++ components/core/edit-form/crud-form-checkbox-list-item.interface.ts
@@ -1,6 +1,6 @@
-import { ButtonComponentConfig } from '../button/button-component-config.interface';
-import { CheckboxList, CheckboxListDirection } from '../checkbox-list';
-import { HeadingComponentConfig } from '../heading/heading-component-config.interface';
+import { ButtonComponentConfig } from '@rapider/angular-components/button';
+import { CheckboxList, CheckboxListDirection } from '@rapider/angular-components/core/checkbox-list';
+import { HeadingComponentConfig } from '@rapider/angular-components/heading';
 import { CrudFormItem } from './crud-form-item.interface';
 
 export interface CrudFormCheckboxListItem extends CrudFormItem {
