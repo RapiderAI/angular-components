3b035f96ac09f7ec6a23b259605e2915db76d6e3
MustafaDemirhan <mustafaa.demirhann.0@gmail.com>
2025-02-04 14:35:29 +0300

    update CrudFormRadioGroupItem

--- components/core/edit-form/crud-form-radio-group-item.interface.ts
+++ components/core/edit-form/crud-form-radio-group-item.interface.ts
@@ -1,15 +1,15 @@
-import { RadioGroupButtonStyle } from '../radio-group/radio-group-button-style.enum';
-import { RadioGroupOptions } from '../radio-group/radio-group-options.interface';
-import { RadioGroupSize } from '../radio-group/radio-group-size.enum';
+import { RadioGroupButtonStyle } from '@rapider/angular-components/core/radio-group';
+import { RadioGroupOptions } from '@rapider/angular-components/core/radio-group';
+import { RadioGroupSize } from '@rapider/angular-components/core/radio-group';
 import { CrudFormItem } from './crud-form-item.interface';
 
 export interface CrudFormRadioGroupItem extends CrudFormItem {
-    options: RadioGroupOptions[];
-    cssStyle: { [key: string]: any };
-    cssClass: string;
-    invalidConfigText: string;
-    size: RadioGroupSize;
-    disabled: boolean;
-    title: string;
-    buttonStyle: RadioGroupButtonStyle;
+  options: RadioGroupOptions[];
+  cssStyle: { [key: string]: any };
+  cssClass: string;
+  invalidConfigText: string;
+  size: RadioGroupSize;
+  disabled: boolean;
+  title: string;
+  buttonStyle: RadioGroupButtonStyle;
 }
