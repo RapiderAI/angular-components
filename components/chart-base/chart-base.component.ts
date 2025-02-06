a23addb64c907b6a7460553082d6d3e9801d69c0
Caner Eğin <cnrgn96@gmail.com>
2025-02-06 15:24:32 +0300

    chart

---
+++ components/chart-base/chart-base.component.ts
@@ -0,0 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Chart } from '@antv/g2';
+import { ChartConfig, convertChartConfigToG2Spec, ChartContent } from '@rapider/angular-components/core/chart';
+import { v4 as uuidv4 } from 'uuid';
+import { RappiderHeadingComponent } from '@rapider/angular-components/heading';
+import { RappiderParagraphComponent } from '@rapider/angular-components/paragraph';
+
+@Component({
+  selector: 'rappider-chart-base',
+  templateUrl: '../chart-base/chart-base.component.html',
+  styleUrls: ['./chart-base.component.scss'],
+  imports:[
+    CommonModule,
+    RappiderHeadingComponent,
+    RappiderParagraphComponent
+  ],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RappiderChartBaseComponent implements OnInit {
+  @Input() config: ChartConfig;
+  @Input() content?: ChartContent;
+
+  defaultHeight = 400;
+  uniqueId: string = 'chartContainer_' + uuidv4().replace(/-/g, '');
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // set a default config
+    const defaultConfig = {};
+
+    // merge default config with user config
+    this.config = { ...defaultConfig, ...this.config };
+
+    // render the chart
+    setTimeout(() => {
+      this.renderChart();
+    }, 100);
+  }
+
+  renderChart(): void {
+    const chart = new Chart({
+      container: this.uniqueId,
+      autoFit: true,
+    });
+    const g2SpecConfig = convertChartConfigToG2Spec(this.config);
+    chart.options(g2SpecConfig);
+    chart.render();
+  }
+}
---
+++ components/core/chart/public-api.ts
@@ -0,0 +1,4 @@
+export * from './chart-config.enum';
+export * from './chart-config.interface';
+export * from './chart-content.interface';
+export * from './chart-utils';
