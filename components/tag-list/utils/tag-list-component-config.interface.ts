d2900a1f2e4e791f2e167cfbadcfff28b6e1bcde
Caner Eğin <cnrgn96@gmail.com>
2025-02-07 13:54:56 +0300

    taglist

--- components/tag-list/utils/tag-list-component-config.interface.ts
+++ components/tag-list/utils/tag-list-component-config.interface.ts
@@ -1,6 +1,6 @@
 import { HeadingComponentConfig } from '@rapider/angular-components/heading';
 import { Tag } from '@rapider/angular-components/core/tag';
-import { TagListDirectionMode } from '../../core/tag-list/tag-list-direction-mode.enum';
+import { TagListDirectionMode } from '@rapider/angular-components/core/tag-list';
 
 export interface TagListComponentConfig {
   titles: HeadingComponentConfig;
