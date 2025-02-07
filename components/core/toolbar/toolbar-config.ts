56b70e885150949235393f498ec5e3980ff3e678
Caner Eğin <cnrgn96@gmail.com>
2025-02-07 11:49:00 +0300

    toolbar

---
+++ components/core/toolbar/toolbar-config.ts
@@ -0,0 +1,10 @@
+import { ButtonComponentConfig } from '@rapider/angular-components/button';
+import { IconType } from '@rapider/angular-components/core/icon';
+
+export const ToolbarConfig: ButtonComponentConfig = {
+  text: 'SHARED.BACK',
+  icon: {
+    name: 'arrow-left',
+    type: IconType.NgZorro
+  }
+};
