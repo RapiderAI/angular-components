aadbaa7e6313e72637dcc1f349ab5555e5f955be
MustafaDemirhan <mustafaa.demirhann.0@gmail.com>
2025-05-21 19:22:28 +0300

    bugfix/message-one

--- components/message-one/message-one.module.ts
+++ components/message-one/message-one.module.ts
@@ -1,12 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RappiderMessageOneComponent } from './message-one.component';
-import { RappiderAvatarModule } from '../avatar/avatar.module';
-import { RappiderHeadingModule } from '../heading/heading.module';
-import { RappiderTextModule } from '../text/text.module';
-import { RappiderLabelModule } from '../label/label.module';
-import { RappiderParagraphModule } from '../paragraph/paragraph.module';
-import { RappiderButtonModule } from '../button/button.module';
 
 
 @NgModule({
